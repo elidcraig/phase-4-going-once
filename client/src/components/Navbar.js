af499a5b2f511ddefc6e9fa7e30d946a1b35c1d1
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-07-11 17:19:14 -0600

    navbar started

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { NavLink } from "react-router-dom";
 function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+	return (
+		<div>
+			<NavLink to="my-bids">
+				<button>My Bids</button>
+			</NavLink>
+			<NavLink to="new-item">
+				<button>New Auction</button>
+			</NavLink>
+			<NavLink to="search">
+				<button>Search</button>
+			</NavLink>
+			<NavLink to="account">
+				<button>Account</button>
+			</NavLink>
+		</div>
+	);
 }
 
-export default Navbar
+export default Navbar;
