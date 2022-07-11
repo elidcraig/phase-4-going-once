ff5e334fd5d89a300bf3d6f6e873f6205645e632
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-07-11 16:45:22 -0600

    frontend initial setup

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :image_url, :starting_bid, :starting_time, :closing_time
+  attributes :id, :name, :description, :image_url, :starting_bid, :starting_time, :closing_time, :category
   has_one :user
 end
---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      header: Logo here, then login/signup button.
+    </div>
+  )
+}
+
+export default Header
