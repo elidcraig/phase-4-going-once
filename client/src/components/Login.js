ad95bbc8a25888e145a61e29e8373ef1e0fc9e78
Eli Craig <elidcraig@gmail.com>
2022-07-12 14:58:55 -0700

    created signup form & state

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,61 +1,13 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'
-import { useRecoilState } from 'recoil';
-import { loginFormState } from '../state/LoginFormState'
-import { currentUserState } from '../state/CurrentUserState'
+import LoginForm from './LoginForm';
+import SignupForm from './SignupForm';
 
 function Login() {
-  let navigate = useNavigate()
-
-  const [loginForm, setLoginForm] = useRecoilState(loginFormState)
-  const [currentUser, setCurrentUser] = useRecoilState(currentUserState)
-
-  const handleFormChange = e => {
-    setLoginForm({...loginForm, [e.target.name]: e.target.value})
-  }
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    const config = {
-      username: loginForm.username,
-      password: loginForm.password,
-      password_confirmation: loginForm.passwordConfirm
-    }
-    console.log(config)
-
-    fetch('/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify(config)
-    })
-      .then(res => {
-        if (res.ok) {
-          res.json().then(user => {
-            setCurrentUser(user.id)
-            navigate('/dashboard', { replace: true })
-          })
-        } else {
-          res.json().then(errors => {
-            console.error(errors)
-          })
-        }
-      })
-  }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label>Username</label>
-        <input type='text' name='username' value={loginForm.username} onChange={handleFormChange}/>
-        <label>Password</label>
-        <input type='password' name='password' value={loginForm.password} onChange={handleFormChange}/>
-        <label>Confirm Password</label>
-        <input type='password' name='passwordConfirm' value={loginForm.passwordConfirm} onChange={handleFormChange}/>
-        <input type='submit' />
-      </form>
+      <LoginForm/>
+      <SignupForm/>
     </div>
   );
 }
---
+++ client/src/components/SignupForm.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useRecoilState } from 'recoil';
+import { signupFormState } from '../state/SignupFormState';
+
+function SignupForm() {
+
+  const [signupForm, setSignupForm] = useRecoilState(signupFormState)
+
+  console.log(signupForm)
+
+  const handleFormChange = e => setSignupForm({ ...signupForm, [e.target.name]: e.target.value })
+  
+  const handleFormSubmit = e => {
+    e.preventDefault()
+  }
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>Username</label>
+        <input type='text' name='username' value={signupForm.username} onChange={handleFormChange}/>
+        <label>Email</label>
+        <input type='text' name='email' value={signupForm.email} onChange={handleFormChange}/>
+        <label>Password</label>
+        <input type='password' name='password' value={signupForm.password} onChange={handleFormChange}/>
+        <label>Confirm Password</label>
+        <input type='password' name='passwordConfirm' value={signupForm.passwordConfirm} onChange={handleFormChange}/>
+        <label>Full Name</label>
+        <input type='text' name='fullName' value={signupForm.fullName} onChange={handleFormChange}/>
+        <label>Physical Address</label>
+        <input type='text' name='address' value={signupForm.address} onChange={handleFormChange}/>
+        <label>Profile Image</label>
+        <input type='text' name='avatarUrl' value={signupForm.avatarUrl} onChange={handleFormChange}/>
+        <input type='submit' />
+      </form>
+    </div>
+  );
+}
+
+export default SignupForm;
