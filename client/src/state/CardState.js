47f63be00b738adcfa651ce2d1bb5c76ee62bf50
Eli Craig <elidcraig@gmail.com>
2022-07-13 10:00:01 -0700

    card state updated with selectors

--- client/src/state/CardState.js
+++ client/src/state/CardState.js
@@ -1,11 +1,43 @@
-import { atom } from 'recoil'
+import { atom, selector } from 'recoil'
+import { searchState } from './SearchState'
+import { categoryState } from './CategoryState'
 
 export const itemsState = atom ({ 
     key: "itemsState",
     default: []
 });
 
-export const auctionState = atom ({
-    key: "auctionState",
-    default: ''
-});
+// export const auctionState = atom ({
+//     key: "auctionState",
+//     default: ''
+// });
+
+const itemsByNameState = selector({
+    key: 'itemsByName',
+    get: ({get}) => {
+        const allItems = get(itemsState)
+        const search = get(searchState)
+        return allItems.filter(item => {
+            if(search === '') {
+                return true
+            } else{
+                return item.name.toLowerCase().includes(search.toLowerCase())
+            }
+        })
+    }
+})
+
+export const itemsByNameAndCatState = selector({
+    key: 'itemsByNameAndCat',
+    get: ({get}) => {
+        const itemsByName = get(itemsByNameState)
+        const category = get(categoryState)
+        return itemsByName.filter(item => {
+            if(category === 'All') {
+                return true
+            } else {
+                return item.category === category
+            }
+        })
+    }
+})
