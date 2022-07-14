c1454a9f743e18f7d8d4bc98b17eb3b966c78f91
Eli Craig <elidcraig@gmail.com>
2022-07-14 13:57:57 -0700

    profile and login links conditionally rendered

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
+import { useRecoilValue } from 'recoil'
+import { currentUserState } from '../state/CurrentUserState'
 
 function Header() {
+
+  const currentUserId = useRecoilValue(currentUserState)
+
   return (
     <div>
       <NavLink to="/">
 				Logo here
 			</NavLink>
       <span>&nbsp;&nbsp;</span>
-      <NavLink to="/login">
-				Login/Signup
-			</NavLink>
-      <span>&nbsp;&nbsp;</span>
-      <NavLink to="account">
-				Profile icon here 
-			</NavLink>
+      {currentUserId ?
+        <NavLink to="/account" >Profile</NavLink> : 
+        <NavLink to="/login">Login/Signup</NavLink>
+      }
     </div>
   )
 }
