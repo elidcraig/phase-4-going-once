7d7215fdec730a7360f7a6980e735197799e7a9c
Eli Craig <elidcraig@gmail.com>
2022-07-14 07:33:28 -0700

    post bid on details page

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -10,8 +10,8 @@ const [makingBid, setMakingBid] = useState(false)
 
 const [newBid, setNewBid] = useRecoilState(newBidState)
 const currentUser = useRecoilValue(currentFullUserState)
-const item = useRecoilValue(detailsState)
-const {name, id, image_url, description, category, starting_bid, closing_time, starting_time, user} = item
+const [item, setItem] = useRecoilState(detailsState)
+const {name, id, image_url, description, category, starting_bid, highest_current_bid, closing_time, starting_time, user} = item
 
 const startingTime = new Date(starting_time)
 const closingTime = new Date(closing_time)
@@ -22,12 +22,34 @@ const userOwnsItem = currentUser.id === user.id
 
 const handleOpenBidForm = () => {
     setMakingBid(true)
-    setNewBid(starting_bid)
+    setNewBid(highest_current_bid + 1)
 }
 
 const handleBidChange = e => setNewBid(e.target.value)
 
+const handleBidSubmit = e => {
+    e.preventDefault()
 
+    const config = {user_id: currentUser.id, item_id: id, amount: newBid}
+
+    fetch('/bids', {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(config)
+    })
+        .then(res => {
+            if(res.ok) {
+                res.json().then(bid => {
+                    setItem({...item, highest_current_bid: bid.amount})
+                    setMakingBid(false)
+                })
+            } else {
+                res.json().then(errors => console.error(errors))
+            }
+        })
+}
 
     return (
     <div className = "profile">
@@ -36,18 +58,19 @@ const handleBidChange = e => setNewBid(e.target.value)
         <ul className="first-profile-ul">
             <li><strong>Description:</strong> {description}</li>
             <li><strong>Category:</strong> {category}</li>
-            <li><strong>Current Bid:</strong> {starting_bid}</li>
+            <li><strong>Current Bid:</strong> {highest_current_bid}</li>
             <li><strong>Bidding Ends On:</strong> {closing_time}</li>
         </ul>
         <form 
             className='new-bid-form'
             style={makingBid ? {} : {display: 'none'}}
+            onSubmit={handleBidSubmit}
         >
-            <input type="number" name='newBid' value={newBid} onChange={handleBidChange}/>
+            <input type="number" name='newBid' value={newBid} min={highest_current_bid + 1} onChange={handleBidChange}/>
+            <input type="submit" />
         </form>
         <button 
-            style={userOwnsItem || !biddingIsOpen ? {display: 'none'} : {}}
-            onClick={handleOpenBidForm}
+            style={userOwnsItem || !biddingIsOpen ? {display: 'none'} : {}} onClick={handleOpenBidForm}
         >Make A Bid!</button>
     </div>
     )
