f9b817313dae596c411f6c25da32d71e97b091e3
alan <alan.martnz@gmail.com>
2022-07-15 11:01:41 -0700

    styled navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,38 +1,14 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
-import { useRecoilValue } from 'recoil'
-import { currentUserState } from '../state/CurrentUserState'
-import { currentFullUserState } from '../state/CurrentUserState'
+
 
 function Header() {
-
-  const currentUserId = useRecoilValue(currentUserState)
-  const currentFullUser = useRecoilValue(currentFullUserState)
-
-  
-console.log(currentUserId)
-console.log("checking for User")
-
-console.log(currentFullUser)
 
   return (
     <header>
       <nav className="nav-bar">
-      <NavLink to="/">
-				<img  src="https://i.imgur.com/32TMUTg.jpg"/>
-			</NavLink>
-      <span>&nbsp;&nbsp;</span>
-      {currentFullUser ?
-        <div>
-        <NavLink to="/account" >
-          { currentFullUser.username }
-          <img className="avatar" src={ currentFullUser.image_url }/> 
-        </NavLink>
-        
-        </div>
-        : 
-        <NavLink to="/login">Login/Signup</NavLink>
-      }
+
+      
       </nav>
     </header>
   )
